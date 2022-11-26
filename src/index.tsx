60268a0f74a04259b0cfa3761b93d51f12854aac
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-11-26 04:04:44 -0300

    Added Material UI Dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { Backdrop as MUIBackdrop } from '@material-ui/core'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-interface Props {
-  text: string
+const useMxSpinnerStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#000000',
+      backgroundColor: 'rgba(255,255,255,0.5)'
+    }
+  })
+)
+
+export interface MxSpinnerProps {
+  open?: boolean
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const MxSpinner: React.FC<MxSpinnerProps> = (props) => {
+  const classes = useMxSpinnerStyles()
+  const { open } = props
+  return (
+    <MUIBackdrop
+      className={classes.backdrop}
+      open={open !== undefined ? open : true}
+    >
+      <CircularProgress color='inherit' />
+    </MUIBackdrop>
+  )
 }
