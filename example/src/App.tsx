60f99c1983f32695de9605437cd2fcf1fc9483c3
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-11-26 03:49:29 -0300

    Clean Project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import { ExampleComponent } from 'mx-spinner'
-import 'mx-spinner/dist/index.css'
 
 const App = () => {
   return <ExampleComponent text="Create React Library Example 😄" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import styles from './styles.module.css'
 
 interface Props {
   text: string
